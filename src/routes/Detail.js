1cfe772cd4b62f5ff94a7a5d3f104c7e2069bfa4
JWHeo96 <eidhk7@gmail.com>
2024-04-25 08:12:10 +0900

    리액트 초급

---
+++ src/routes/Detail.js
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+    const { id } = useParams();
+    const getMovie = async () => {
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        console.log(json);
+    }
+
+    useEffect(() => {
+        getMovie();
+    }, []);
+
+    return (
+        <div>
+            <h1>Detail</h1>
+            <p><Link to="/">Home</Link></p>
+        </div>
+    )
+}
+
+export default Detail;
